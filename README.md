ac7aba853606f409fc3f1b74de12b644ac442636
storngfornt <au0038173@gmail.com>
2024-06-10 21:31:48 +0600

    added readme

--- README.md
+++ README.md
@@ -1,8 +1,41 @@
-# React + Vite
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## DineEase
+DineEase is a comprehensive platform that allows users to browse, like, and request meals. Admins can manage upcoming and available meals, track user requests, and handle reviews. With secure user authentication, detailed payment history, and responsive design, it offers a seamless experience for both users and administrators.
+## Live site url
+https://hostel-management-30afb.web.app/
 
-Currently, two official plugins are available:
+or
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+https://hostel-management-30afb.firebaseapp.com/
+## Admin Idp
+
+email: admin@gmail.com
+pass: Adminn
+## Features
+
+- User Authentication: Secure login and registration for users and admin.
+
+- Upcoming Meals: Display upcoming meals, only premium users can like meals.
+
+- Available Meals: Browse available meals, with like functionality for all users.
+
+- Meal Requests: Users can request meals, subject to subscription status.
+
+- Review System: Users can post reviews, view review counts, and manage their own reviews.
+
+- Admin Meal Management: Admins can add new meals, update meal statuses, and manage meal requests.
+
+- Meal Like System: Tracks likes on meals, ensuring users can like each meal only once.
+
+- Payment History: Displays payment history for logged-in users with relevant messages for no data.
+
+- Serve Meals: Admins can mark requested meals as delivered and track user requests.
+
+- Search Functionality: Server-side search by username and email for efficient data management.
+
+- Responsive Design: Optimized for both desktop and mobile devices for seamless user experience.
+
+
+## Server side repo
+
+https://github.com/programming-hero-web-course1/b9a12-server-side-strongfornt
--- src/Dashboard/AdminDashboard/UpcomingMeals/UpcomingMeals.jsx
+++ src/Dashboard/AdminDashboard/UpcomingMeals/UpcomingMeals.jsx
@@ -1,25 +1,56 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Helmet } from "react-helmet-async";
 
 import Select from "react-select";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
 import UpcomingTable from "./UpcomingTable";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
 
 export default function UpcomingMeals() {
   const axiosSecure = useAxiosSecure();
+  const axiosPublic = useAxiosPublic();
+  const [itemPerPages,setItemPerPages] = useState(10);
+  const [count,setCount] = useState(0);
+  const [currentPage,setCurrentPage] = useState(1);
   const [selectedOption, setSelectedOption] = useState(null);
 
   //use tanstack for fetching data ==============================
   const { data: mealsData = [] } = useQuery({
-    queryKey: ["upcomingMeals", selectedOption],
+    queryKey: ["upcomingMeals", selectedOption,currentPage,itemPerPages],
     queryFn: async () => {
       const { data } = await axiosSecure.get(
-        `/upcomingMeals?likes=${selectedOption}`
+        `/upcomingMeals?likes=${selectedOption}$page=${currentPage}&size=${itemPerPages}`
       );
       return data;
     },
   });
+  //count the total documents ============
+  useEffect(()=>{
+    const getCount = async() =>{
+      const {data} = await axiosPublic.get(`/upcoming_meals_count`)
+      setCount(data?.upcomingMealsCount)
+    }
+    getCount()
+  },[axiosPublic])
+  const numberOfPages = Math.ceil(count / itemPerPages);
+  const paginationCount = [
+    ...Array(numberOfPages)
+      .keys()
+  ].map(c => c+1);
+  const handlePrev =() =>{
+    if(currentPage > 1){
+        setCurrentPage(currentPage -1)
+        
+    }
+    
+}
+const handleNext = () =>{
+     if(currentPage < paginationCount?.length){
+        setCurrentPage(currentPage +1) 
+     }
+}
+  //count the documents end ==========================
   //use tanstack for fetching data end ==============================
 
   //select option for the like and review ==================
@@ -100,6 +131,72 @@ export default function UpcomingMeals() {
           </div>
         </div>
       </section>
+      {/* main section end ============================= */}
+      <div className="mb-6 mt-6 flex items-center  justify-center">
+       
+       <nav
+         aria-label="Pagination"
+         className="inline-flex -space-x-px bg-gray-50  rounded-md shadow-sm"
+       >
+         <button
+         onClick={handlePrev}
+           type="button"
+           className="inline-flex items-center px-2 py-2 text-sm font-semibold border rounded-l-md dark:border-gray-300"
+         >
+           <span className="sr-only">Previous</span>
+           <svg
+             xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 20 20"
+             fill="currentColor"
+             aria-hidden="true"
+             className="w-5 h-5 text-black"
+           >
+             <path
+               fillRule="evenodd"
+               d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+               clipRule="evenodd"
+             ></path>
+           </svg>
+         </button>
+      {
+       paginationCount?.map((c,i)=>  <button
+          key={i}
+           type="button"
+           aria-current="page"
+           className={`inline-flex items-center px-4 py-2  text-sm font-semibold border ${currentPage === c ?'bg-teal-400 text-white':'text-black'   } 
+           dark:border-gray-300`}
+           
+           
+           onClick={()=> setCurrentPage(c)}
+         >
+           {c}
+         </button>
+       )
+      }
+
+         <button
+         onClick={handleNext}
+           type="button"
+           className="inline-flex items-center px-2 py-2 text-sm font-semibold border rounded-r-md dark:border-gray-300"
+         >
+           <span className="sr-only">Next</span>
+           <svg
+             xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 20 20"
+             fill="currentColor"
+             aria-hidden="true"
+             className="w-5 h-5 text-black "
+           >
+             <path
+               fillRule="evenodd"
+               d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+               clipRule="evenodd"
+             ></path>
+           </svg>
+         </button>
+       </nav>
+       
+     </div>
     </>
   );
 }
